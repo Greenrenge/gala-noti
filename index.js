35210861e7aebe20ba6e18f3f7a9bc59413a5286
greenrenge <greenrenge@gmail.com>
2022-02-08 12:10:24 +0700

    fix scope

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ async function main() {
   const checker = async function () {
     try {
       const { code, data } = await execute("gala-node stats")
+
+      if (code != 0) {
+        return [false, "process exit with non-zero return"]
+      }
+      try {
+        const parsed = JSON.parse(data)
+        if (+get(parsed, "summary.nodesOnline", 0) !== 1) {
+          return [false, "NODE IS OFFLINE !!!!!!!"]
+        }
+        if (
+          get(parsed, "summary.currentVersion") !==
+          get(parsed, "summary.latestVersion")
+        ) {
+          return [false, "NODE NEEDS TO UPDATE VERSION !!"]
+        }
+        return [true]
+      } catch (err) {
+        return [false, "cannot parse the stdout"]
+      }
     } catch (err) {
       return [false, "process stopped or failed to run"]
-    }
-
-    if (code != 0) {
-      return [false, "process exit with non-zero return"]
-    }
-    try {
-      const parsed = JSON.parse(data)
-      if (+get(parsed, "summary.nodesOnline", 0) !== 1) {
-        return [false, "NODE IS OFFLINE !!!!!!!"]
-      }
-      if (
-        get(parsed, "summary.currentVersion") !==
-        get(parsed, "summary.latestVersion")
-      ) {
-        return [false, "NODE NEEDS TO UPDATE VERSION !!"]
-      }
-      return [true]
-    } catch (err) {
-      return [false, "cannot parse the stdout"]
     }
   }
   const task = async () => {
