c0f856e39cc0846110f5a6e755e4411b9f6d1522
greenrenge <greenrenge@gmail.com>
2022-02-08 12:07:16 +0700

    complete

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const { execute } = require("./cmd")
 const { send } = require("./fireNoti")
+const get = require("lodash/get")
 
 var CronJob = require("cron").CronJob
 
 async function main() {
-  const task = async function () {
-    const { code, data } = execute("gala-node stats")
-    console.log(code)
+  const checker = async function () {
+    try {
+      const { code, data } = await execute("gala-node stats")
+    } catch (err) {
+      return [false, "process stopped or failed to run"]
+    }
+
+    if (code != 0) {
+      return [false, "process exit with non-zero return"]
+    }
+    try {
+      const parsed = JSON.parse(data)
+      if (+get(parsed, "summary.nodesOnline", 0) !== 1) {
+        return [false, "NODE IS OFFLINE !!!!!!!"]
+      }
+      if (
+        get(parsed, "summary.currentVersion") !==
+        get(parsed, "summary.latestVersion")
+      ) {
+        return [false, "NODE NEEDS TO UPDATE VERSION !!"]
+      }
+      return [true]
+    } catch (err) {
+      return [false, "cannot parse the stdout"]
+    }
+  }
+  const task = async () => {
+    const [isSuccess, errorMsg] = await checker()
+    if (!isSuccess) {
+      send(`ตื่นๆๆๆๆ มีเรื่องแล้ว ${errorMsg}`).catch((err) => {
+        console.log("cannot send the message to line noti")
+      })
+    }
   }
   const job = new CronJob(
     "0 * * * *", //every hour
