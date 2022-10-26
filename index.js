3edddd76cf4dff522302bd75a56afd5d26b960d9
greenrenge <greenrenge@gmail.com>
2022-10-26 12:19:35 +0700

    noti v2

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const mapStdout = (stdout) => {
     .split("\n")
     .findIndex((line) => line.includes("Name") && line.includes("Status"))
   const lines = stdout.split("\n").slice(gtIndex + 1)
-  return lines.map((l) => ({
-    name: l.split(/[\s\t]+/gi)[0],
-    status: l.split(/[\s\t]+/gi)[1],
-    cpu: l.split(/[\s\t]+/gi)[2],
-    memory: l.split(/[\s\t]+/gi)[3],
-    runningTime: l.split(/[\s\t]+/gi)[4],
-  }))
+  return lines
+    .map((l) => ({
+      name: l.split(/[\s\t]+/gi)[0],
+      status: l.split(/[\s\t]+/gi)[1],
+      cpu: l.split(/[\s\t]+/gi)[2],
+      memory: l.split(/[\s\t]+/gi)[3],
+      runningTime: l.split(/[\s\t]+/gi)[4],
+    }))
+    .filter((a) => a.name)
 }
 async function main() {
   const checker = async function () {
@@ -25,76 +27,30 @@ async function main() {
       if (code != 0 && code != 999) {
         return [false, "process exit with non-zero return"]
       }
-      console.log(data)
-      const parsed = mapStdout(data)
-      console.log(parsed)
 
-      // if (code == 999) {
-      //   // timeout
-      //   // const parsedArr = data.split("\n").map((d) => {
-      //   //   try {
-      //   //     return JSON.parse(d)
-      //   //   } catch (err) {
-      //   //     return undefined
-      //   //   }
-      //   // })
-      //   send(`node check stats failed due to \n${JSON.stringify(data)}`)
-      //   return [false, "TIMEOUT", true]
-      // }
+      if (code == 999) {
+        //timeout
+        send(`node check stats failed :: \n${data}`)
+        return [false, "TIMEOUT", true]
+      }
 
-      // try {
-      //   const parsed = JSON.parse(data)
-
-      //   if (!get(parsed, "summary")) {
-      //     send(
-      //       `node restart needed \n${Object.entries(parsed)
-      //         .map(([k, v]) => ` [${k}]=${v} `)
-      //         .join("\n")}`,
-      //     )
-      //     if (enableAutoReboot) {
-      //       await rebootCheck()
-      //     }
-      //     console.log("done reboot check#2")
-      //     return [false, "REBOOT DUE TO NOT FIND SUMMARY", true]
-      //   }
-
-      //   console.log("json got normal", parsed)
-
-      //   if (+get(parsed, "summary.nodesOnline", 0) !== 1) {
-      //     return [false, "NODE IS OFFLINE !!!!!!!"]
-      //   }
-      //   if (
-      //     get(parsed, "summary.currentVersion") !==
-      //     get(parsed, "summary.latestVersion")
-      //   ) {
-      //     return [false, "NODE NEEDS TO UPDATE VERSION !!"]
-      //   }
-      //   return [true]
-      // } catch (err) {
-      //   console.log("error parse std ", err)
-      //   return [false, "cannot parse the stdout"]
-      // }
-    } catch (err) {}
-    //   console.log("error command failed ", err)
-    //   send("process stopped or failed to run maybe need to re-login")
-
-    //   // re-auth
-    //   await exec("gala-node config device", {
-    //     echo: true,
-    //     capture: true,
-    //     noTimeout: true,
-    //     handler: (stdout) => {
-    //       console.log("handler", stdout.toString())
-    //       send(stdout.toString())
-    //     },
-    //   })
-
-    //   return [
-    //     false,
-    //     "process stopped or failed to run maybe need to re-login",
-    //     true, // not send
-    //   ]
-    // }
+      try {
+        const parsed = mapStdout(data)
+        if (parsed.length == 0) {
+          send(`cannot parse data stdout to array :: \n${data}`)
+          return [false, "CANNOT PARSE STDOUT", true]
+        } else if (parsed.filter((a) => a.status !== "Running").length) {
+          send(`Some node is not running :: \n${data}`)
+          return [false, "SOME NODE NOT RUNNING", true]
+        }
+        return [true]
+      } catch (err) {
+        console.log("error parse std ", err)
+        return [false, "cannot parse the stdout"]
+      }
+    } catch (err) {
+      return [false, "FAIL TO EXEC"]
+    }
   }
   const task = async () => {
     const [isSuccess, errorMsg, notSend] = (await checker()) || [
